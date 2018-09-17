d17b1c0118e9cd0be0f5315837f5b8f448534993
subham-tiwari <43341674+subham-tiwari@users.noreply.github.com>
2018-09-17 18:13:59 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -42,7 +42,7 @@ y[:,1] #printing the first colummn
 print(y)  #print the whole thing 
 print(y[0,:])  #give the first row
 
-
+print("size of the array")
 y.shape  #size of the array
 
 
@@ -50,3 +50,70 @@ y.shape  #size of the array
 print(y[0,1]) # Slicing 
 print(y[0]) # indexing
 print(y[:,1])
+
+print("how to create a matrix of same value like one two's and three's")
+
+one=np.zeros([2,3])
+one
+
+nines=np.full([3,3] , 9)
+print(nines)
+
+
+#how to make 2D array of matrix
+
+x1=np.array([[1,2,3,4,5],[5,1,8,2,3]])
+x2=np.array([[1,2,3,4,6],[6,7,8,3,10]])
+
+
+print(x1)
+print("")
+print(x2)
+
+#add the matrices 
+x3=x1+x2
+x4=np.add(x1,x2)
+
+#subtract 
+x5=x2-x1
+x6=np.subtract(x4,x1)
+
+#Randon numbers to generate with a given size of the matrix as well 
+
+randm=np.random.random((1,2))
+randm
+
+
+#arrange the matrix
+
+arrange=np.arange(0,12)
+
+#Apply condition to the values of the matrix 
+
+ex1=np.random.random((2,3))
+ex1
+
+checkgreator=(ex1>1)
+checkgreator
+
+checklesser=(ex1<0.5)
+
+
+#Get the meaen value of the matrix is easy by using the function 
+
+meanarray=np.mean(checklesser)
+meanarray
+meanofx1row=np.mean(x1,axis=0)
+
+#Sort the X1
+
+sortx1=x1.sort()
+
+#median of the given numbers
+medianx1=np.median(x1 , axis=1)
+medianx1
+
+#Unique values in the matrix
+
+uniquex1=np.unique(x1)
+print(uniquex1)
